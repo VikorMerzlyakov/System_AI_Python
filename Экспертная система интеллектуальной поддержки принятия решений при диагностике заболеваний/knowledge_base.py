57177a6be04a2e24216585632b44b2a7c639f65a
VikorMerzlyakov <111169794+VikorMerzlyakov@users.noreply.github.com>
2025-10-29 14:15:15 +0300

    Добавил интерфейс и доработал БЗ

--- Экспертная система интеллектуальной поддержки принятия решений при диагностике заболеваний/knowledge_base.py
+++ Экспертная система интеллектуальной поддержки принятия решений при диагностике заболеваний/knowledge_base.py
@@ -6,8 +6,8 @@ class KnowledgeBase:
     def addSympthom(self, symptom, disease):
         if symptom not in self.symptoms:
             self.symptoms[symptom] = []
-        self.sympthoms[symptom].append(disease)
-        if disease not in self.disease:
+        self.symptoms[symptom].append(disease)
+        if disease not in self.diseases:
             self.diseases[disease] = []
         self.diseases[disease].append(symptom)
 
@@ -20,3 +20,20 @@ class KnowledgeBase:
                         diseaseScores[disease] = 0
                     diseaseScores[disease] += 1
         return sorted(diseaseScores.items(), key=lambda x: x[1], reverse=True)
+
+    def load_data(self):
+        # Пример загрузки данных в базу знаний
+        data = [
+            ("жар", "Грипп"),
+            ("кашель", "Грипп"),
+            ("головная боль", "Грипп"),
+            ("жар", "Пневмония"),
+            ("кашель", "Пневмония"),
+            ("головная боль", "Мигрень"),
+            ("головная боль", "Травма"),
+            ("бол живота", "Гастроэнтерит"),
+            ("жидкий стул", "Гастроэнтерит"),
+            ("жар", "Гастроэнтерит"),
+        ]
+        for symptom, disease in data:
+            self.addSympthom(symptom, disease)
--- Экспертная система интеллектуальной поддержки принятия решений при диагностике заболеваний/ui.py
+++ Экспертная система интеллектуальной поддержки принятия решений при диагностике заболеваний/ui.py
@@ -1,1 +1,44 @@
 import tkinter as tk
+from inference_engine import InferenceEngine
+from knowledge_base import KnowledgeBase
+
+class DiagnosisApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Экспертная система диагностики заболевания")
+
+        self.knowledgeBase = KnowledgeBase()
+        self.knowledgeBase.load_data()
+        self.inferenceEngine = InferenceEngine(self.knowledgeBase)
+
+        self.initUi()
+
+    def initUi(self):
+        self.simptomLabel = tk.Label(self.root, text="Введите симптомы (через запятую): ")
+        self.simptomLabel.pack(pady=10)
+
+        self.simptomEntry = tk.Entry(self.root, width=50)
+        self.simptomEntry.pack(pady=5)
+
+        self.diagnoseButton = tk.Button(self.root, text="Диагностировать", command=self.diagnose)
+        self.diagnoseButton.pack(pady=10)
+
+        self.resultLabel = tk.Label(self.root, text="")
+        self.resultLabel.pack(pady=10)
+
+    def diagnose(self):
+        symptoms = self.simptomEntry.get().split(',')
+        symptoms = [simptom.strip() for simptom in symptoms]
+        diseases = self.inferenceEngine.diagnose(symptoms)
+        resultText = "Возможные заболевания: \n"
+        if diseases:
+            for  diasease, score in diseases:
+                resultText += f"{diasease} (Совпадений: {score})\n"
+        else:
+            resultText = "Нет совпадений для введенных симптомов."
+        self.resultLabel.config(text=resultText)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = DiagnosisApp(root)
+    root.mainloop()
